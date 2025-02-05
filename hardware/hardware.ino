9c7f1231b9b6805b121a8c86ee72b43d909197c4
RachWill314 <rachelle.williams314@gmail.com>
2025-02-05 09:55:55 -0500

    hardware

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -10,6 +10,9 @@
 
 // ADD YOUR IMPORTS HERE
 
+#include <Adafruit_Sensor.h>
+#include "DHT.h"
+#include <FastLED.h>
 
 
 #ifndef _WIFI_H 
@@ -38,19 +41,22 @@
 #define ARDUINOJSON_USE_DOUBLE      1 
 
 // DEFINE THE CONTROL PINS FOR THE DHT22 
+#define DHTPIN  33
+#define DHTTYPE DHT22
 
-
+#define NUM_LEDS 7
+#define DATA_PIN  32
 
 
 // MQTT CLIENT CONFIG  
-static const char* pubtopic      = "620012345";                    // Add your ID number here
-static const char* subtopic[]    = {"620012345_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
-static const char* mqtt_server   = "local";         // Broker IP address or Domain name as a String 
+static const char* pubtopic      = "620155671";                    // Add your ID number here
+static const char* subtopic[]    = {"620155671_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
+static const char* mqtt_server   = "broker.emqx.io";         // Broker IP address or Domain name as a String 
 static uint16_t mqtt_port        = 1883;
 
 // WIFI CREDENTIALS
-const char* ssid       = "YOUR_SSID";     // Add your Wi-Fi ssid
-const char* password   = "YOUR_PASSWORD"; // Add your Wi-Fi password 
+const char* ssid       = "MonaConnect";     // Add your Wi-Fi ssid
+const char* password   = ""; // Add your Wi-Fi password 
 
 
 
@@ -81,8 +87,9 @@ double calcHeatIndex(double Temp, double Humid);
 
 
 /* Init class Instances for the DHT22 etcc */
- 
-  
+DHT dht(DHTPIN, DHTTYPE);
+
+CRGB leds[NUM_LEDS];
 
 //############### IMPORT HEADER FILES ##################
 #ifndef NTP_H
@@ -97,22 +104,23 @@ double calcHeatIndex(double Temp, double Humid);
 
 
 void setup() {
-  Serial.begin(115200);  // INIT SERIAL  
+  Serial.begin(9600);  // INIT SERIAL  
 
   // INITIALIZE ALL SENSORS AND DEVICES
   
   /* Add all other necessary sensor Initializations and Configurations here */
-
+  dht.begin(); // INITIALIZE DHT22 SENSOR
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); // INITIALIZE LED STRIP
 
   initialize();     // INIT WIFI, MQTT & NTP 
-  // vButtonCheckFunction(); // UNCOMMENT IF USING BUTTONS INT THIS LAB, THEN ADD LOGIC FOR INTERFACING WITH BUTTONS IN THE vButtonCheck FUNCTION
+  //vButtonCheckFunction(); // UNCOMMENT IF USING BUTTONS INT THIS LAB, THEN ADD LOGIC FOR INTERFACING WITH BUTTONS IN THE vButtonCheck FUNCTION
  }
   
 
 
 void loop() {
     // put your main code here, to run repeatedly:       
-    vTaskDelay(1000 / portTICK_PERIOD_MS);    
+    vTaskDelay(1000 / portTICK_PERIOD_MS);   
 }
 
 
@@ -143,24 +151,33 @@ void vUpdate( void * pvParameters )  {
    
           // 1. Read Humidity and save in variable below
           double h = 0;
+          h = dht.readHumidity();
            
           // 2. Read temperature as Celsius   and save in variable below
           double t = 0;    
- 
+          t = dht.readTemperature(); // Read temperature as Celsius 
 
           if(isNumber(t)){
               // ##Publish update according to ‘{"id": "student_id", "timestamp": 1702212234, "temperature": 30, "humidity":90, "heatindex": 30}’
 
               // 1. Create JSon object
+              JsonDocument doc;
               
               // 2. Create message buffer/array to store serialized JSON object
+              char message[800] = {0};
               
               // 3. Add key:value pairs to JSon object based on above schema
+              doc["id"] = "620155671";
+              doc["timestamp"] = getTimeStamp();
+              doc["temperature"] = t;
+              doc["humidity"] = h;
+              doc["heatindex"] = calcHeatIndex(t,h);
 
               // 4. Seralize / Covert JSon object to JSon string and store in message array
-               
-              // 5. Publish message to a topic sobscribed to by both backend and frontend                
+              serializeJson(doc, message);
 
+              // 5. Publish message to a topic sobscribed to by both backend and frontend                
+              publish("620155671", message);
           }
 
           
@@ -210,10 +227,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   if (strcmp(type, "controls") == 0){
     // 1. EXTRACT ALL PARAMETERS: NODES, RED,GREEN, BLUE, AND BRIGHTNESS FROM JSON OBJECT
+    int nodes = doc["nodes"];
+    int red = doc["red"];
+    int green = doc["green"];
+    int blue = doc["blue"];
+    int brightness = doc["brightness"];
 
     // 2. ITERATIVELY, TURN ON LED(s) BASED ON THE VALUE OF NODES. Ex IF NODES = 2, TURN ON 2 LED(s)
-
+    for(int i = 0; i < nodes; i++){
+      leds[i] = CRGB(red,green,blue);
+    }
+    FastLED.show(); // DISPLAY LED(s)
     // 3. ITERATIVELY, TURN OFF ALL REMAINING LED(s).
+    for(int i = nodes; i < NUM_LEDS; i++){
+      leds[i] = CRGB(0,0,0);
+    }
+    FastLED.show(); // DISPLAY LED(s)
    
   }
 }
@@ -239,16 +268,21 @@ bool publish(const char *topic, const char *payload){
 //***** Complete the util functions below ******
 
 double convert_Celsius_to_fahrenheit(double c){    
-    // CONVERTS INPUT FROM °C TO °F. RETURN RESULTS     
+    // CONVERTS INPUT FROM °C TO °F. RETURN RESULTS  
+  return (c * 9/5) + 32;
 }
 
 double convert_fahrenheit_to_Celsius(double f){    
-    // CONVERTS INPUT FROM °F TO °C. RETURN RESULT    
+    // CONVERTS INPUT FROM °F TO °C. RETURN RESULT  
+  return (f - 32) * 5/9;  
 }
 
 double calcHeatIndex(double Temp, double Humid){
     // CALCULATE AND RETURN HEAT INDEX USING EQUATION FOUND AT https://byjus.com/heat-index-formula/#:~:text=The%20heat%20index%20formula%20is,an%20implied%20humidity%20of%2020%25
-  
+  double c1 = -42.379, c2 = 2.04901523, c3 = 10.14333127, c4 = -0.22475541;
+  double c5 = -6.83783e-3, c6 = -5.481717e-2, c7 = 1.22874e-3, c8 = 8.5282e-4, c9 = -1.99e-6;
+    
+  return c1 + (c2 * Temp) + (c3 * Humid) + (c4 * Temp * Humid) + (c5 * Temp * Temp) + (c6 * Humid * Humid) + (c7 * Temp * Temp * Humid) + (c8 * Temp * Humid * Humid) + (c9 * Temp * Temp * Humid * Humid);
 }
  
 
